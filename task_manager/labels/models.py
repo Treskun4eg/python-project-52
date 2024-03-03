385fe44e4e484546b72eddc1179548d790fb47c9
Treskun4eg <wagamamanowelli@gmail.com>
2024-03-03 15:48:58 +0100

    add new app labels

---
+++ task_manager/labels/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class TimestampedModel(models.Model):
+    """An abstract model with a pair of timestamps."""
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+# Create your models here.
+class LabelsModel(TimestampedModel):
+
+    name = models.CharField(max_length=150,
+                            unique=True,
+                            verbose_name=_('Name'),)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('Label')
+        verbose_name_plural = _('Labels')
---
+++ task_manager/labels/views.py
@@ -0,0 +1,64 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from task_manager.mixins import UserEditPermissionMixin, DeleteProtectionMixin
+from django.urls import reverse_lazy
+
+
+# from .models import User
+from .models import LabelsModel
+
+from django.views.generic import CreateView, UpdateView, ListView, DeleteView
+from task_manager.labels.forms import LabelsForm
+from django.utils.translation import gettext_lazy as _
+
+
+# Create your views here.
+class LabelsIndexView(ListView):
+
+    template_name = 'labels/index.html'
+    model = LabelsModel
+    context_object_name = 'labels'
+    extra_context = {
+        'title': _('Labels')
+    }
+
+
+class LabelCreateFormView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
+
+    template_name = 'form.html'
+    model = LabelsModel
+    form_class = LabelsForm
+    success_url = reverse_lazy('labels_index')
+    success_message = _('Label successfully created')
+    extra_context = {
+        'title': _('Create Label'),
+        'button_text': _('Create'),
+    }
+
+
+class LabelUpdateFormView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+
+    model = LabelsModel
+    form_class = LabelsForm
+    template_name = 'form.html'
+    success_url = reverse_lazy('labels_index')
+    success_message = 'Label changed successfully'
+    extra_context = {
+        'title': _('Change of label'),
+        'button_text': _('Update'),
+    }
+
+
+class LabelDeleteFormView(SuccessMessageMixin, LoginRequiredMixin,
+                           DeleteProtectionMixin, DeleteView):
+
+    model = LabelsModel
+    template_name = 'labels/delete.html'
+    protected_message = _('You cannot delete label while it is in use')
+    protected_url = reverse_lazy('labels_index')
+    success_url = reverse_lazy('labels_index')
+    success_message = _('Label deleted successfully')
+    extra_context = {
+        'title': _('Deleting a label'),
+        'button_text': _('Delete'),
+    }
