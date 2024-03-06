ba51fc099db7a595b7d281fa7025a2347100c2df
Treskun4eg <wagamamanowelli@gmail.com>
2024-03-06 22:48:00 +0100

    fix filter tasks

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -2,12 +2,22 @@ from django_filters import FilterSet, filters
 from .models import TasksModel
 from task_manager.labels.models import LabelsModel
 from django.utils.translation import gettext_lazy as _
+from django import forms
 
 
 class TaskFilter(FilterSet):
 
     labels = filters.ModelChoiceFilter(queryset=LabelsModel.objects.all(),
                                        label=_('Label'))
+
+    self_tasks = filters.BooleanFilter(label=_('Self tasks'),
+                                       method='get_self_tasks',
+                                       lookup_expr='isnull',
+                                       widget=forms.CheckboxInput)
+
+    def get_self_tasks(self, queryset, name, value):
+        lookup = queryset.filter(author=self.request.user)
+        return lookup if value else queryset
 
     class Meta:
         model = TasksModel
