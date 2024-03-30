a489c44caef9ff06c0be81bcfd4c6406952a482e
Treskun4eg <wagamamanowelli@gmail.com>
2024-03-30 22:36:16 +0100

    fix hexlet tests

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -103,10 +103,9 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        default='sqlite:///db.sqlite3')
 }
 
 # Password validation
